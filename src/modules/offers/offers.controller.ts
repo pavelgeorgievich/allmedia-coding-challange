4811708e4934c1fdbe6a4635458c538c04f1c898
Pavel Kutakov <pavel.kutakov@infiniteleap.net>
2023-12-06 13:57:25 +0300

    Refactor OffersModule to use Aggregated Providers
    The prior structure used individual primary and secondary provider dependencies in the OffersModule, which led to redundancy and less readable code. Refactored this structure by introducing an AggregatedProviders functionality. Now all external providers are injected using AGGREGATED_PROVIDERS_INJECTION_TOKEN, resulting in a cleaner, easier to maintain code structure. Simultaneously, to segregate provider-related functionality, secondary and primary providers have been moved to their standalone ProvidersModule.

--- src/modules/offers/offers.controller.ts
+++ src/modules/offers/offers.controller.ts
@@ -1,35 +1,18 @@
 import { Controller, Inject, Logger } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
+import { AGGREGATED_PROVIDERS_INJECTION_TOKEN } from '../providers/providers.module';
 import { IOffer } from './offer.entity';
-import { PRIMARY_PROVIDER_INJECTION_TOKEN } from './primary-provider/primary.provider.module';
-import { SECONDARY_PROVIDER_INJECTION_TOKEN } from './secondary-provider/secondary.provider.module';
-import { ExternalProvidersService } from './utils/external.providers.service';
+import { ExternalProvidersService } from '../utils/external.providers.service';
 import { OfferDTO } from './offer.dto';
 import { validate } from 'class-validator';
-import { Payload as PrimaryPayload } from './primary-provider/primary.provider.interface';
-import { Payload as SecondaryPayload } from './primary-provider/primary.provider.interface';
 
 @Controller('offers')
 export class OffersController {
   private readonly logger = new Logger(OffersController.name);
-  private providers: ExternalProvidersService<unknown, IOffer>[] = [];
   constructor(
-    @Inject(PRIMARY_PROVIDER_INJECTION_TOKEN)
-    private primaryProviderService: ExternalProvidersService<
-      PrimaryPayload,
-      IOffer
-    >,
-    @Inject(SECONDARY_PROVIDER_INJECTION_TOKEN)
-    private secondaryProviderService: ExternalProvidersService<
-      SecondaryPayload,
-      IOffer
-    >,
-  ) {
-    this.providers = [
-      this.primaryProviderService,
-      this.secondaryProviderService,
-    ];
-  }
+    @Inject(AGGREGATED_PROVIDERS_INJECTION_TOKEN)
+    private providers: ExternalProvidersService<unknown, IOffer>[],
+  ) {}
 
   @Cron('*/10 * * * * *')
   async offersJob() {
